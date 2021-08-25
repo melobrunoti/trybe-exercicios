6365d4e17542083f26e8f19d005f1e7708cee0ae
Bruno Melo <melobrunoti@gmail.com>
2021-08-25 18:10:56 -0300

    commit inicial

---
+++ fundamentos/bloco-4-introducao-a-javascipt/variaveis-introucao/scripts.js
@@ -0,0 +1,11 @@
+const myName = "Bruno";
+const birthCity = "Itauna";
+let birthYear = 1997;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear)
