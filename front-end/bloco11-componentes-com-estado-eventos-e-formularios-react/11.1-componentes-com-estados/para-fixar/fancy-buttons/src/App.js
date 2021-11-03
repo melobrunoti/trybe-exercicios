ad5263308dac5d392288b7397ca1be6462147ba6
Bruno Melo <melobrunoti@gmail.com>
2021-11-03 16:01:53 -0300

    Para fixar 3: this.state/setState()

--- front-end/bloco11-componentes-com-estado-eventos-e-formularios-react/11.1-componentes-com-estados/para-fixar/fancy-buttons/src/App.js
+++ front-end/bloco11-componentes-com-estado-eventos-e-formularios-react/11.1-componentes-com-estados/para-fixar/fancy-buttons/src/App.js
@@ -5,19 +5,33 @@ import React from 'react';
 class App extends React.Component {
   constructor(){
     super()
+
     this.handleClick1 = this.handleClick1.bind(this)
+    this.handleClick2 = this.handleClick2.bind(this)
+
+    this.state = {
+      numeroDeCliques: 0,
+      numeroDeCliques2: 0
+    }
   }
 
  handleClick1(){
-    console.log(this);
-    console.log('clicou no botao 1')
+    this.setState((estadoAtual, _props)=>({
+      numeroDeCliques: estadoAtual.numeroDeCliques + 1
+    }))
+  }
+
+  handleClick2(){
+    this.setState((estadoAtual, _props)=>({
+      numeroDeCliques2:   estadoAtual.numeroDeCliques2 + 1
+    }))
   }
 
   render(){
   return (
     <div className="App">
-      <button onClick={this.handleClick1}>Button - 1</button>
-
+      <button onClick={this.handleClick1}>{this.state.numeroDeCliques}</button>
+      <button onClick={this.handleClick2}>{this.state.numeroDeCliques2}</button>
     </div>
   );
 }
